127e57b5bb60c28f3a2b4e237d7de8f3b82422b9
ceciliacortess <ceciliacortesvelert@gmail.com>
2023-02-05 20:07:32 +0100

    proyecto final

---
+++ src/proyectoentornos/ProyectoEntornos.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectoentornos;
+
+import vista.FPrincipal;
+
+/**
+ *
+ * @author CECILIA
+ */
+public class ProyectoEntornos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FPrincipal().setVisible(true);
+            }
+        });
+    }
+    
+}
